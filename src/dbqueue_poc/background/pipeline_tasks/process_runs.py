71ffc3eeefc48566779d5b67c614248d426888b7
Victor Skvortsov <vds003@gmail.com>
2026-02-04 16:56:49 +0500

    Drop in-memory requeue

--- src/dbqueue_poc/background/pipeline_tasks/process_runs.py
+++ src/dbqueue_poc/background/pipeline_tasks/process_runs.py
@@ -2,14 +2,16 @@ import asyncio
 import math
 import random
 import uuid
-from asyncio.queues import QueueEmpty
 from datetime import timedelta
 from typing import cast
 
 from sqlalchemy import and_, or_, select, update
 from sqlalchemy.orm import load_only, selectinload
 
-from dbqueue_poc.background.pipeline_tasks.base import PipelineItem, ProcessingResult
+from dbqueue_poc.background.pipeline_tasks.base import (
+    PipelineItem,
+    ProcessingResult,
+)
 from dbqueue_poc.db import get_db, get_session_ctx
 from dbqueue_poc.models import JobModel, RunModel
 from dbqueue_poc.schemas import JobStatus, RunStatus
@@ -27,8 +29,9 @@ class RunPipeline:
 
     Highlights:
         * All features of the simplest pipeline (`PlacementGroupPipeline`), plus
-        * Workers lock all related items (`JobModel`). If not all items can be locked,
-          then the run is requeued (put back in the queue).
+        * Workers lock all related items (`JobModel`). If not all items can be locked, the run is requeued:
+          it's kept locked in the DB but not heartbeated. This allows signaling
+          other pipelines that the item is locked and process it later via regular pipeline path.
         * Heartbeating related items is not needed. Stale locked related items can be
           processed only by the same pipeline (due to `lock_owner` check) so they'll be picked up
           when processing the main item again.
@@ -39,7 +42,6 @@ class RunPipeline:
         workers_num: int = 25,
         queue_lower_limit_factor: float = 0.5,
         queue_upper_limit_factor: float = 2.0,
-        requeue_upper_limit_factor: float = 1.0,
         min_processing_interval: timedelta = timedelta(seconds=5),
         lock_timeout: timedelta = timedelta(seconds=20),
         heartbeat_trigger: timedelta = timedelta(seconds=10),
@@ -47,15 +49,12 @@ class RunPipeline:
         self._workers_num = workers_num
         self._queue_lower_limit_factor = queue_lower_limit_factor
         self._queue_upper_limit_factor = queue_upper_limit_factor
-        self._requeue_upper_limit_factor = requeue_upper_limit_factor
         self._queue_desired_minsize = math.ceil(workers_num * queue_lower_limit_factor)
         self._queue_maxsize = math.ceil(workers_num * queue_upper_limit_factor)
-        self._requeue_maxsize = math.ceil(workers_num * requeue_upper_limit_factor)
         self._min_processing_interval = min_processing_interval
         self._lock_timeout = lock_timeout
         self._heartbeat_trigger = heartbeat_trigger
         self._queue = asyncio.Queue[PipelineItem](maxsize=self._queue_maxsize)
-        self._requeue = asyncio.Queue[PipelineItem](maxsize=self._requeue_maxsize)
         self._heartbeater = RunHeartbeater(
             lock_timeout=self._lock_timeout,
             heartbeat_trigger=self._heartbeat_trigger,
@@ -70,7 +69,6 @@ class RunPipeline:
         self._workers = [
             RunWorker(
                 queue=self._queue,
-                requeue=self._requeue,
                 heartbeater=self._heartbeater,
             )
             for _ in range(self._workers_num)
@@ -250,61 +248,21 @@ class RunWorker:
     def __init__(
         self,
         queue: asyncio.Queue[PipelineItem],
-        requeue: asyncio.Queue[PipelineItem],
         heartbeater: RunHeartbeater,
-        queue_check_delay: float = 0.1,
     ) -> None:
         self._queue = queue
-        self._requeue = requeue
         self._heartbeater = heartbeater
-        self._queue_check_delay = queue_check_delay
 
     async def start(self):
         while True:
-            queue = random.choice([self._queue, self._requeue])
+            item = await self._queue.get()
             try:
-                item = queue.get_nowait()
-            except QueueEmpty:
-                await asyncio.sleep(self._queue_check_delay)
-                continue
-            requeue = False
-            try:
-                processing_result = await self.process(item)
-                requeue = processing_result.requeue
+                await self.process(item)
             except Exception:
                 logger.exception("Unexpected exception when processing item")
-            if requeue:
-                # Requeue mechanism allows workers to process the item later while keeping it locked,
-                # e.g. to wait for related resources to be unlocked.
-                try:
-                    self._requeue.put_nowait(item)
-                    continue
-                except asyncio.QueueFull:
-                    await self.unlock(item)
             await self._heartbeater.untrack(item)
 
-    async def unlock(self, item: PipelineItem):
-        async with get_session_ctx() as session:
-            res = await session.execute(
-                update(RunModel)
-                .where(
-                    RunModel.id == item.id,
-                    RunModel.lock_token == item.lock_token,
-                )
-                .values(
-                    lock_expires_at=None,
-                    lock_token=None,
-                    lock_owner=None,
-                    last_processed_at=get_current_datetime(),
-                )
-            )
-            if res.rowcount == 0:  # pyright: ignore[reportAttributeAccessIssue]
-                logger.warning(
-                    "Failed to unlock the run: lock_token changed."
-                    " The run is expected to be processed and updated on another fetch iteration."
-                )
-
-    async def process(self, item: PipelineItem) -> ProcessingResult:
+    async def process(self, item: PipelineItem):
         logger.debug("Processing run %s", item.id)
         async with get_session_ctx() as session:
             # This is an example of how a pipeline can lock related resources.
@@ -329,7 +287,7 @@ class RunWorker:
                     "Failed to process run: lock_token mismatch."
                     " The run is expected to be processed and updated on another fetch iteration."
                 )
-                return ProcessingResult(requeue=False)
+                return
 
             res = await session.execute(
                 select(JobModel)
@@ -353,7 +311,25 @@ class RunWorker:
                     "Failed to lock run %s jobs. The run will be requeued and processed later.",
                     run_model.id,
                 )
-                return ProcessingResult(requeue=True)
+                now = get_current_datetime()
+                res = await session.execute(
+                    update(RunModel)
+                    .where(
+                        RunModel.id == run_model.id,
+                        RunModel.lock_token == run_model.lock_token,
+                    )
+                    .values(
+                        lock_expires_at=now,
+                        last_processed_at=now,
+                    )
+                )
+                if res.rowcount == 0:  # pyright: ignore[reportAttributeAccessIssue]
+                    logger.warning(
+                        "Failed to reset lock_expires_at: lock_token changed."
+                        " The run is expected to be processed and updated on another fetch iteration."
+                    )
+                    return
+                return
 
             for job_model in locked_job_models:
                 job_model.lock_expires_at = run_model.lock_expires_at
@@ -384,6 +360,5 @@ class RunWorker:
                     "Failed to update the run after processing: lock_token changed."
                     " The run is expected to be processed and updated on another fetch iteration."
                 )
-                return ProcessingResult(requeue=False)
+                return
         logger.debug("Processed run %s", item.id)
-        return ProcessingResult(requeue=False)
