c2b5894a1e7427f4a491b556d8537ec2905915ab
Victor Skvortsov <vds003@gmail.com>
2026-02-05 15:22:08 +0500

    Clean up repo for PR

--- src/dbqueue_poc/main.py
+++
@@ -1,4 +0,0 @@
-from dbqueue_poc.app import create_app, register_routes
-
-app = create_app()
-register_routes(app)
--- src/dbqueue_poc/utils/common.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime, timezone
-
-
-def get_current_datetime() -> datetime:
-    return datetime.now(tz=timezone.utc)
